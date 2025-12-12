f64b64057b73d8ae8efe90ce1e847beccf6c07ca
narayana-srimanth <narayanaudayagiri88@gmail.com>
2025-12-12 13:30:18 +0530

    changing frontend for checking correct deployment

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -72,7 +72,7 @@ function Login({ onLogin }) {
               size="large"
               startIcon={<LoginIcon />}
             >
-              Sign In
+              Sign Innnnn
             </Button>
             {error && (
               <Typography variant="body2" color="error">
