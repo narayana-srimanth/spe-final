5d3e6a1fec25771e445c51f9a527ef65c1e2fa51
narayana-srimanth <122380854+narayana-srimanth@users.noreply.github.com>
2025-12-13 11:29:32 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -72,7 +72,7 @@ function Login({ onLogin }) {
               size="large"
               startIcon={<LoginIcon />}
             >
-              For Sign In working fine.
+              Sign In.
             </Button>
             {error && (
               <Typography variant="body2" color="error">
